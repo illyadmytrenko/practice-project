ffead77f71e69f2eb14c43c08be2dda9a9cd9200
illyadmytrenko <illadmitrenko666@gmail.com>
2025-04-18 11:56:10 +0300

    added movies.json, added express server with routes to get movies and movie by id

---
+++ server/routes/movies.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const router = Router();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const moviesPath = path.join(__dirname, "..", "movies.json");
+
+console.log(moviesPath);
+
+router.get("/", (req, res) => {
+  fs.readFile(moviesPath, "utf-8", (err, data) => {
+    if (err)
+      return res.status(500).json({ error: "Failed to read movies file" });
+
+    const movies = JSON.parse(data);
+    res.json(movies);
+  });
+});
+
+router.get("/:id", (req, res) => {
+  const movieId = req.params.id;
+
+  fs.readFile(moviesPath, "utf-8", (err, data) => {
+    if (err)
+      return res.status(500).json({ error: "Failed to read movies file" });
+
+    const movies = JSON.parse(data);
+    const movie = movies.find((m) => m.id === movieId);
+
+    if (!movie) return res.status(404).json({ error: "Movie not found" });
+
+    res.json(movie);
+  });
+});
+
+export default router;
