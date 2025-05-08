3f58cf66af7dfc9ebff5cae1d1f58f4414096cd9
illyadmytrenko <98984669+illyadmytrenko@users.noreply.github.com>
2025-05-08 18:28:51 +0300

    Update movies-context.jsx

--- src/context/movies-context.jsx
+++ src/context/movies-context.jsx
@@ -8,7 +8,8 @@ export function MoviesProvider({ children }) {
 
   useEffect(() => {
     axios
-      .get("http://localhost:5050/api/movies")
+      // .get("http://localhost:5050/api/movies")
+      .get("http://localhost:5000/api/movies")
       .then((res) => setMovies(res.data))
       .catch((err) => console.error("Помилка при завантаженні фільмів:", err));
   }, []);
