9ad0b37d7a0912eaf352d072cb9049ce8793e667
illyadmytrenko <illadmitrenko666@gmail.com>
2025-05-18 11:23:38 +0300

    fix: use process.env.PORT for Render compatibility

--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ import moviesRoutes from "./routes/movies.js";
 import userRoutes from "./routes/users.js";
 import scheduleRoutes from "./routes/schedule.js";
 import paymentRoutes from "./routes/payment.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-//const PORT = 5050;
-const PORT = 5000;
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(json());
